04e99a6e6b14149a3aeb779cd63e8e239ac375d5
ibrahimohid <ibrahimohid77@gmail.com>
2023-11-11 09:52:22 +0600

    comparison_operator added

---
+++ practice-python/comparison_operator.py
@@ -0,0 +1,10 @@
+# message = "Hello";
+# message_two = "hello bangladesh";
+# print(message == message_two);
+
+number_one = 10;
+number_two = 5;
+number_three = 10
+print(number_one > number_two);
+print(number_one >= number_three);
+print(number_one < number_three);
