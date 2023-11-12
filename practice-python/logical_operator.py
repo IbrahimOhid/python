ff2f0244303fcd71a50c9becf7f3edda0b560e03
ibrahimohid <ibrahimohid77@gmail.com>
2023-11-12 20:04:57 +0600

    logical_operator added

---
+++ practice-python/logical_operator.py
@@ -0,0 +1,15 @@
+number_one = 30;
+number_two = 20;
+
+print(number_one < number_two);
+
+message_one = "Hello Bangladesh !!";
+message_two = "Hello Bangladesh !!";
+
+print(message_one == message_two);
+
+print(number_one < number_two and message_one == message_two);
+
+print(number_one < number_two or message_one == message_two);
+
+print(not message_one == message_two);
