00fbd86803c8c8b017aff6387f2c9fd4261a8eaf
ibrahimohid <ibrahimohid77@gmail.com>
2023-11-12 20:16:13 +0600

    identity_operator added

---
+++ practice-python/identity_operator.py
@@ -0,0 +1,17 @@
+# number_one = 3;
+# number_two = 3;
+
+# print(id(number_one));
+# print(id(number_two));
+
+#print(number_one is number_two);
+
+# my_list = [];
+# my_list_one = [];
+#
+# print(my_list is my_list_one);
+
+number = 3;
+number_one = 3;
+
+print(number is not number_one);
