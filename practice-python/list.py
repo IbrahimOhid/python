b565b9122089db5f2128301c7bb0c07c92683502
ibrahimohid <ibrahimohid77@gmail.com>
2023-11-12 20:55:18 +0600

    list added

---
+++ practice-python/list.py
@@ -0,0 +1,40 @@
+# my_car = ["Bmw", "audi", "toyota"];
+# print(my_car);
+
+# countries = ["Bangladesh", "India", "Pakistan", "africa"];
+#
+# print(countries[3]);
+# print(countries[-4]);
+
+#MODIFY LIST
+
+# countries = ["India", "pakistan", "africa", "jordan"];
+# print(countries);
+# countries[3] = "Bangladesh";
+# print(countries);
+#
+# countries[-3] = "Norway";
+# print(countries);
+
+#REMOVE LIST
+
+# countries = ["Bangladesh", "Pakistan", "Indonesia", "Afganistan", "Oman"];
+# print(countries);
+# del countries[2];
+# print(countries);
+# del countries[-1];
+# print(countries);
+
+# fruits = ["Banana", "Orange", "Apple", "Water Melon"];
+# fruits.pop();
+# # remove_item = fruits.pop();
+# # print(remove_item);
+# print(fruits);
+# fruits.pop(2);
+# print(fruits);
+
+#REMOVE METHOD
+
+word = ["i", "c", "e", "w", "c", "x"];
+word.remove("c");
+print(word);
