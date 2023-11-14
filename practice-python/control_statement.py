1ae32b5b84d9604b81a34c9ef704d800dbe0b446
ibrahimohid <ibrahimohid77@gmail.com>
2023-11-14 22:23:07 +0600

    control statement added

---
+++ practice-python/control_statement.py
@@ -0,0 +1,90 @@
+# if Statement 🔽
+
+# shirt_price = 500;
+# if shirt_price == 500:
+#     print("I will buy this shirt.");
+
+# if else statement 🔽
+
+# pant_price = 800;
+# if pant_price == 600:
+#     print("I will buy this pant");
+# else:
+#     print("I will return form home");
+
+# if else, else if statement 🔽
+
+# total_marks = 33;
+#
+# if total_marks >= 80:
+#     print("A+");
+# elif total_marks >= 60 and total_marks < 80:
+#     print("B")
+# elif total_marks >= 40 and total_marks < 60:
+#     print("C");
+# else:
+#     print("F");
+
+# apple_price = 270;
+#
+# if apple_price >= 300:
+#     print("I will Buy");
+# elif apple_price >= 250 and apple_price < 280:
+#     print("I will think this buy");
+# else:
+#     print("I will return form My Sweet Home");
+
+# user_area = "Mirpur";
+# total_price = 100;
+#
+# if user_area in ["Mirpur", "Dhaka", "Gulsan"]:
+#     if total_price >= 600:
+#         print("Shipping Cost Free");
+#     elif  total_price >= 300  and total_price < 600:
+#         print("Shipping Cost 60")
+#     else:
+#         print("Shipping cost 150");
+# elif user_area in ["Chittagong", "Noakhali"]:
+#     if total_price >= 400:
+#         print("Shipping Cost Free");
+#     elif total_price >= 200 and total_price < 400:
+#         print("Shipping Cost 40");
+#     else:
+#         print("Shipping Cost 120");
+#
+# else:
+#     print("Shipping Not Available");
+
+# short hand if 🔽
+
+# y = 10;
+# if y == 10 : z = 30;
+# print(z)
+
+# short hand if else 🔽
+
+# a = 15;
+# a = 30;
+# b = 10 if a == 15 else 100;
+# print(b);
+
+# for loop 🔽
+
+# fruits_list = ["Banana", "Orange", "Apple", "Papaya"];
+# for fruit in fruits_list:
+#     print(fruit);
+
+# numbers = [1, 3, 10, 29, 33, 99];
+# for all_num in numbers:
+#     print(all_num);
+
+# break continue 🔽
+
+# for i in range(0, 10):
+#     print(i);
+
+for i in range(1, 11):
+    if i == 6:
+        break;
+    print(i)
+print("Stop");
