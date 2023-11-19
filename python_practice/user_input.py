d0c7c1ed0f168083803911e9757deadfc00bc644
ibrahimohid <ibrahimohid77@gmail.com>
2023-11-19 21:16:19 +0600

    input statement  added

---
+++ python_practice/user_input.py
@@ -0,0 +1,12 @@
+# name = input("Enter Your Name");
+# age = input("Enter Your Age");
+# gpa = input("Enter Your GPA");
+#
+# print("-------------");
+#
+# print("Name: " + name);
+# print("Age :" + age);
+# print("GPA :" + gpa);
+
+
+
