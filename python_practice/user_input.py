d1e6e7b09a59c35eac00363515029689a50228d0
ibrahimohid <ibrahimohid77@gmail.com>
2023-11-20 21:10:10 +0600

    if else method added

--- python_practice/user_input.py
+++ python_practice/user_input.py
@@ -8,5 +8,72 @@
 # print("Age :" + age);
 # print("GPA :" + gpa);
 
+# type costing 🔽
+
+# num1 = int(input("Enter a Number: "));
+# num2 = int(input("Enter a Number: "));
+#
+# result = num1 + num2;
+#
+# print("Result = ", result);
+
+# shape calculation 🔽
+#    🔽🔽🔽
+# Triangle area calculation
+
+# base = float(input("Enter a Base Number: "));
+# height = float(input("Enter a Height Number: "));
+# area = 0.5 * base * height;
+# print("Area = ", area);
+
+# square area calculation 🔽
+
+# length = float(input("Enter a Length Number: "));
+# area = length * length;
+# print("Area = ", area);
+
+# circle area calculation 🔽
+
+# radius = float(input("Enter a Radius Number: "));
+# area = 3.1416 * radius * radius;
+# print("Area of Circle =", area);
+
+# math Function 🔽
+
+# from math import *
+
+# print(max(30, 20));
+# print(min(10, 2, 33));
+# print(abs(-99));
+# print(pow(2, 3));
+# print(sqrt(22));
+# print(round(3.5));
+# print(floor(5.8));
+# print(ceil(3.99));
+
+# string formatting 🔽
+
+# num1 = 20;
+# num2 = 30;
+# print(f"{num1} + {num2} = {num1 + num2}");
+
+# print("MOhamamd ibrahim", end=" ");
+# print("01834598910");
+
+# if , else 🔽
+
+# mark = 20;
+# if mark > 30:
+#     print("Pass");
+# else:
+#     print("Fail");
+
+# num1 = 0;
+# num2 = 10;
+# if num1 > num2:
+#     print("Num1 BIG");
+# else:
+#     print("Num2 SMALL");
+
 
 
