ec8a001aa77fef05ba77bc7065ed6bc297a7c789
ibrahimohid <ibrahimohid77@gmail.com>
2023-11-22 20:48:41 +0600

    mutable & immutable added

---
+++ practice-python/mutable_immutable.py
@@ -0,0 +1,24 @@
+# immutabel 🔻
+
+# my_text = "Hello World";
+# print(my_text[1]);
+# my_text[1] = "C";
+# print(my_text)
+
+# my_num = (1, 3, 4, 6);
+# print(my_num[1]);
+# my_num[1] = 5;
+# print(my_num);
+
+#mutabel 🔻
+
+# my_num = [1, 2, 4, 6, 7, 9];
+# print(my_num[2]);
+# my_num[3] = 100;
+# print(my_num);
+
+# numbers = (1, 2, 3, [4, 5, 6], 7, 8);
+# print(numbers[3]);
+# numbers[3][1] = 100;
+# print(numbers);
+
