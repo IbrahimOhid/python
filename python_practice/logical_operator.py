7a27370ca5938cfabf48433bc219af15d3c6d8cd
ibrahimohid <ibrahimohid77@gmail.com>
2023-11-22 21:53:21 +0600

    looping  added

--- python_practice/logical_operator.py
+++ python_practice/logical_operator.py
@@ -18,3 +18,17 @@
 #     print("Vowel = {}".format(vowel));
 # else:
 #     print("It is not Vowel");
+
+mark = 4;
+if 80 <= mark <= 100:
+    print("A+");
+elif 70 <= mark <= 79:
+    print("A");
+elif 60 <= mark <= 69:
+    print("B");
+elif 50 <= mark <= 59:
+    print("C");
+elif 40 <= mark <= 49:
+    print("D");
+else:
+    print("F")
---
+++ python_practice/looping.py
@@ -0,0 +1,50 @@
+# while Loop ⏬
+
+# i = 1;
+# while i <= 10:
+#     print(i);
+#     i = i + 1;
+# print("END");
+
+# Odd NUmber
+
+# i = 0;
+# while i <= 100:
+#     print(i);
+#     i = i + 2;
+# print("Program End");
+#
+# Even NUmber
+#
+# i = 1;
+# while i <= 100:
+#     print(i);
+#     i = i + 2;
+# print("Program End");
+
+# sum = 0;
+# i = 1;
+# while i <= 10:
+#     sum = sum + i;
+#     i = i + 1;
+# print(sum);
+
+# break 🔽
+
+# i = 1;
+# while i <= 100:
+#     if i == 20:
+#         break;
+#     print(i);
+#     i = i + 1;
+# print("Hello")
+
+# continue 🔽
+
+i = 1;
+while i <= 100:
+    if i == 20:
+        continue;
+    print(i);
+    i = i + 1;
+print("Hello")
