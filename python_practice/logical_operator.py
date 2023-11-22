2d7806381f794cdd7d99eb3eea8e90027cef2060
ibrahimohid <ibrahimohid77@gmail.com>
2023-11-22 21:19:44 +0600

    logical operator and or  added

---
+++ python_practice/logical_operator.py
@@ -0,0 +1,20 @@
+# And operator 🔻
+
+# num1 = 1110;
+# num2 = 440;
+# num3 = 30;
+#
+# if num1 > num2 and num1 > num3:
+#     print(num1);
+# elif num2 > num1 and num2 > num3:
+#     print(num2);
+# else:
+#     print(num3);
+
+# Or operator 🔻
+
+# vowel = str(input("Enter a Vowel : "));
+# if vowel == 'a' or vowel == 'e' or vowel == 'i' or vowel == 'o' or vowel == 'u' or vowel == 'A' or vowel == 'E' or vowel == 'I' or vowel == 'O' or  vowel == 'U':
+#     print("Vowel = {}".format(vowel));
+# else:
+#     print("It is not Vowel");
