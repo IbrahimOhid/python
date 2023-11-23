a4ab8670b8a87f0838907a1c91236ec969f56afa
ibrahimohid <ibrahimohid77@gmail.com>
2023-11-23 20:21:39 +0600

    nested function added

--- practice-python/recursion_function.py
+++ practice-python/recursion_function.py
@@ -8,9 +8,22 @@
 #     my_fun(n + 1)
 # my_fun(1);
 
-def fun(n):
-    if n > 10:
-        return
-    print(n)
-    fun(n + 1)
-fun(1)
+# def fun(n):
+#     if n > 10:
+#         return
+#     print(n)
+#     fun(n + 1)
+# fun(1)
+
+# nested Function
+
+# def my_fun():
+#     print("Hello Bangladesh")
+#
+#
+# def my_func(func):
+#     func()
+#     print("Hello Cox's Bazar")
+#
+#
+# my_func(my_fun)
