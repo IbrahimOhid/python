fe681bac3ed8237a3b915f3655d85e9adad1fef9
ibrahimohid <ibrahimohid77@gmail.com>
2023-11-24 21:09:44 +0600

    list added

---
+++ python_practice/list.py
@@ -0,0 +1,22 @@
+# subject = ["html", "css", "bootstrap", "tailwind css", "javascript"];
+# print(subject[3])
+#print("html" in subject)
+# print("python" not in subject)
+# subject = subject + ["python"]
+# print(subject)
+# print(subject[-1])
+
+# subject = ["html", "css", "bootstrap", "tailwind css", "javascript"];
+# subject.append("java")
+# subject.append(["python", "c++"]);
+# subject.insert(1, "java")
+# subject.remove("bootstrap")
+# subject.sort();
+# subject.reverse()
+# subject.pop()
+# subject = subject.index("css")
+# print(subject)
+
+# num = [1, 2, 3, 5, 3, 4, 3, 4];
+# print(num.count(3))
+
