c0490469e1aa80a781bd2dab31a3c2626dfae9a0
ibrahimohid <ibrahimohid77@gmail.com>
2023-11-28 20:43:50 +0600

    stack & Queue added

--- python_practice/Dictionaries.py
+++ python_practice/Dictionaries.py
@@ -24,8 +24,34 @@
 #print(8 in numbers)
 # print(4 in numbers)
 
-num1 = {1, 2, 3, 4, 5, 4, 7};
-num2 = set([6, 7, 8, 9, 4, 10]);
-print(num1 | num2)
-print(num1 & num2)
-print(num1 - num2)
+# num1 = {1, 2, 3, 4, 5, 4, 7};
+# num2 = set([6, 7, 8, 9, 4, 10]);
+# print(num1 | num2)
+# print(num1 & num2)
+# print(num1 - num2)
+
+# stack
+
+# book_name = [];
+# book_name.append("HTMl");
+# book_name.append("CSS");
+# book_name.append("Javascript");
+# book_name.append("Python");
+#
+# book_name.pop();
+# book_name.pop();
+# book_name.pop();
+# book_name.pop();
+# if not book_name:
+#     print("Not Found");
+#
+# print(book_name)
+
+# Queue
+
+from  collections import  deque;
+
+bank = deque(["Ibrahim", "ohid", "jisan"]);
+print(bank)
+bank.popleft()
+print(bank)
