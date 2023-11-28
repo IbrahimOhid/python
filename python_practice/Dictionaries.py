f39734d6a979c38d613430a7653e789cd4bdc2e8
ibrahimohid <ibrahimohid77@gmail.com>
2023-11-28 20:29:49 +0600

    set added

--- python_practice/Dictionaries.py
+++ python_practice/Dictionaries.py
@@ -7,6 +7,25 @@
 # # print(studentId[104]);
 # print(studentId.get(105, 'Not a Valid Key'));
 
-name = ("Mohammad", "ibrahim", "Ohid");
-print(name[0:2]);
-# print(name[2])
+# name = ("Mohammad", "ibrahim", "Ohid");
+# print(name[0:2]);
+# # print(name[2])
+
+# set
+
+# num = {1, 2, 3, 4, 4};
+# print(num)
+
+# numbers = [4, 5, 6, 7, 7];
+# numbers = set(numbers)
+# numbers.add(8)
+# numbers.remove(6)
+
+#print(8 in numbers)
+# print(4 in numbers)
+
+num1 = {1, 2, 3, 4, 5, 4, 7};
+num2 = set([6, 7, 8, 9, 4, 10]);
+print(num1 | num2)
+print(num1 & num2)
+print(num1 - num2)
