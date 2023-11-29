bb59b3ad142bd98f7ee117028f6fa802cdc56178
ibrahimohid <ibrahimohid77@gmail.com>
2023-11-29 22:19:52 +0600

    read added

--- python_practice/lambda.py
+++ python_practice/lambda.py
@@ -16,6 +16,34 @@
 # result = list(map(square,num));
 # print(result)
 
-num = [1, 2, 3, 4, 5];
-result = list(filter(lambda x: x%2==0, num))
-print(result)
+# num = [1, 2, 3, 4, 5];
+# result = list(filter(lambda x: x%2==0, num))
+# print(result)
+
+# Zip function
+
+# id = [101, 102, 103, 104, 105, 106];
+# name = ["Ibrahim", "ohid", "mohammad", "ebna", "osman", "ebnol"];
+# print(list(zip(id, name)))
+
+# Recursion
+
+# def num(n):
+#     if n == 1:
+#         return 1
+#     else:
+#         return  n * num(n-1)
+# print(num(4))
+
+# file
+
+# file = open("text.txt", "r");
+# text = file.read();
+# print(text)
+
+# file = open("text.txt", "a");
+# file.write("Ibrahim");
+# print(file)
+
+file = open('text.txt', 'w');
+file.write("Hello World")
