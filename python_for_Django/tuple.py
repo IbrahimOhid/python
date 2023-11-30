0200db56d6b4b54b02df621457ff103e50585930
ibrahimohid <ibrahimohid77@gmail.com>
2023-11-30 21:31:17 +0600

    tuple added

---
+++ python_for_Django/tuple.py
@@ -0,0 +1,98 @@
+# Tuple = "Mohammad", 26, "Mechanical Engineer", 3.05;
+# print(Tuple)
+
+# text = ("ibrahim", (2, 3));
+# print(text[1][0])
+
+# Text = ([1, 2, 3, 4], ["ibrahim", "ohid"]);
+# print(Text[1][0])
+#text = Text[0][3] = "ohid";
+# text = Text[1][0] = "rahim";
+# print(text);
+
+# tp = ("ibrahim",);
+# print(tp)
+
+# num = 12,
+# print(num)
+
+# tuple1 = 1, 2, "Hello";
+# print(tuple1);
+# x, y, z = tuple1;
+# print(x, y, z);
+
+# tuple2 = ("a", "b", "C", "d", "e", "f");
+# print(len(tuple2));
+
+# tuple3 = (["Mohammad", "Ibrahim", "ohid"], (1, 2, 4, 5, 6));
+# for item in tuple3:
+# print(item);
+
+# tuple4 = ("P", "y", "t", "H", "o", "N");
+# for i in range(5):
+#     print(tuple4[i])
+
+# tuple1 = (10, 20, 30, 40, 50);
+# position = tuple1.index(50);
+# print(position)
+
+# student_name = ("MOhammad", "ohid", "ebna", "osman", "ibrahim");
+# name_position = student_name.index("ibrahim");
+# print(name_position)
+
+# tuple1 = (10, 20, 30, 40, 50, 60, 70, 80);
+# #print(100 in tuple1);
+# print(10 in tuple1);
+
+# tuple1 = (0, 1, 2, 3, 4, 5);
+# t_conv = list(tuple1);
+# t_conv.append(7);
+# print(t_conv)
+
+# tuple1 = (1, 2, 3, 4, 5)
+# tuple2 = (3, 4, 5, 6, 7)
+# print(tuple1 + tuple2);
+# print(max(tuple1));
+# print(min(tuple2));
+
+# tuple1 = (10, 20, 30, 40, 50)
+# Expected output:
+# (50, 40, 30, 20, 10)
+# print(tuple1[::-1])
+
+# tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
+# print(tuple1[1][1])
+
+# tuple1 = (50,);
+# print(tuple1)
+
+# tuple1 = (10, 20, 30, 40);
+# a, b, c, d = tuple1;
+# print(a, b, c, d);
+
+# tuple1 = (11, 22)
+# tuple2 = (99, 88)
+# print("tuple1: {}".format(tuple2));
+# print("tuple2: {}".format(tuple1));
+
+# tuple1 = (11, 22, 33, 44, 55, 66);
+# tuple1 = tuple1[3:5];
+# print(tuple1)
+
+# tuple1 = (11, [22, 33], 44, 55);
+# tuple1[1][0] = 222;
+# print(tuple1)
+
+# tuple1 = (50, 10, 60, 70, 50)
+# print(tuple1.count(50));
+
+
+# aTuple = (100,)
+# print(aTuple * 2)
+
+# tuple1 = (1120, 'a')
+# print(max(tuple1))
+
+aTuple = (100, 200, 300, 400, 500)
+aTuple.pop(2)
+print(aTuple)
