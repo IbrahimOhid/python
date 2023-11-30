4c39991d893e005469d3320660e5557d667b710c
ibrahimohid <ibrahimohid77@gmail.com>
2023-11-30 21:50:32 +0600

    set added

---
+++ python_for_Django/set.py
@@ -0,0 +1,10 @@
+# set1 = {"mohammad", "ohid", "ibrahim", "ohid"};
+# print(set1);
+
+# set2 = ("Mohammad");
+# print(set(set2));
+
+# set1 = {"abcdefghij"};
+# set2 = {"cdezklhj"};
+# set = set1 | set2;
+# print(set);
