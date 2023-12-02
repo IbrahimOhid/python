3a8d8a7eda4fadb79b73742731da0cc83ad23866
ibrahimohid <ibrahimohid77@gmail.com>
2023-12-02 09:14:51 +0600

    set added

--- python_for_Django/set.py
+++ python_for_Django/set.py
@@ -8,3 +8,75 @@
 # set2 = {"cdezklhj"};
 # set = set1 | set2;
 # print(set);
+
+# set = {1, 2, 4, "ohid", "ibrahim"};
+# print(set)
+
+#book_set = ["Bangla", "English", "Math", "Islam"];
+# book_set = ("Bangla", "English", "Math", "Islam");
+# book_set = set(book_set);
+# print(book_set)
+# print(type(book_set))
+
+# number_list = (1, 2, 3, 4, 5, 2, 1);
+# number_list = set(number_list);
+# print(number_list)
+
+# name_number = ["mohammad", "ibrahim", "mohammad", "ohid", 1, 3, 2, 2, 4, 1, 5, 7];
+# print(set(name_number))
+
+# sample_set = {'Mark', 'Jessa', (35, 78, 92)}
+# print(sample_set)
+
+# book_set = ("Bangla", "English", "Math", "Islam");
+# for book in book_set:
+#     print(book)
+
+# book_set = ("Bangla", "English", "Math", "Islam");
+# if "English" in book_set:
+#     print("yes")
+# else:
+#     print("not Found");
+
+# print("Math" in book_set)
+
+# color_set = {"red", "yellow", "blue", "green"};
+# color_set1 = {"pink", "black", "navy blue", "green", "red", "Yellow"};
+# color = color_set & color_set1;
+# color = color_set | color_set1;
+# print(color)
+
+# set1 = {10, 20, 30, 40, 50}
+# set2 = {60, 70, 10, 30, 40, 80, 20, 50}
+#
+# print(set1.issubset(set2))
+# print(set2.issuperset(set1))
+
+# set1 = {10, 20, 30, 40}
+# set2 = {50, 20, "10", 60}
+#
+# set3 = set1.union(set2)
+# print(set3)
+
+# set1 = {"Yellow", "Orange", "Black"}
+# set2 = {"Orange", "Blue", "Pink"}
+#
+# set1.difference_update(set2)
+# print(set1)
+
+# set1 = {"Yellow", "Orange", "Black"}
+# set2 = {"Orange", "Blue", "Pink"}
+#
+# set3 = set2.difference(set1)
+# print(set3)
+
+# aSet = {1, 'PYnative', ('abc', 'xyz'), True}
+# print(aSet)
+
+# sampleSet = {"Yellow", "Orange", "Black"}
+# sampleSet.discard("Blue")
+# print(sampleSet)
+
+# sampleSet = {"Yellow", "Orange", "Black"};
+# sampleSet1 = sampleSet.discard("Orange");
+# print(sampleSet1)
