5abda8631f1cb08a933f6797dd3c993102120b61
ibrahimohid <ibrahimohid77@gmail.com>
2023-12-03 20:41:47 +0600

    comprehension added

---
+++ python_for_Django/comprehension.py
@@ -0,0 +1,56 @@
+# my_list = [1, 2, 3, 4, 5];
+# print(my_list)
+# new_list = [];
+
+# for x in my_list:
+#     new_list.append(x*x);
+# print(new_list)
+
+# final_list = [i*i for i in my_list if i % 2 == 0];
+# print(final_list);
+
+# list = [1, 2, 3, 4, 5, 7, 9, 11, 14, 16, 20, 23, 27, 30];
+# print(list);
+
+# final_list = [i*i for i in list];
+# print(final_list);
+
+# total_list = [i*i for i in list if i % 2 == 0];
+# print(total_list);
+
+# total = [i*i for i in list if i % 2 == 1];
+# print(total);
+
+# my_list = [1, 2, 3, 4, 5, 6];
+# new_list = [i * i for i in my_list];
+# print(new_list);
+# new_dic = {i: i*i for i in my_list};
+# print(new_dic);
+# new_set = {i**3 for i in my_list};
+# print(sorted(new_set));
+
+# new_tuple = tuple(i**4 for i in my_list);
+# print(new_tuple);
+
+# newTlist = [(i, i**4)for i in my_list];
+# print(newTlist);
+
+# my_dic = {'name': 'ibrahim', 'country': 'bangladesh', 'age' : 26};
+# new_list = [key for key, value in my_dic.items()];
+# new_list = [(key, value) for key, value in my_dic.items()];
+# print(new_list);
+
+# my_list = [letter.upper() for letter in "BanglaDesH"];
+# my_list = [letter.lower() for letter in "BanglaDesH"];
+# print(my_list);
+
+# my_matrix = [[0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3]];
+# # print(my_matrix);
+# new_matrix = [];
+# for i in range(3):
+#     new_matrix.append([]);
+#     for j in range(4):
+#         new_matrix[i].append(j);
+# print(new_matrix);
+
+
