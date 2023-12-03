7058a5008d8fc255e44fc799d6b59b8b5b30fd63
ibrahimohid <ibrahimohid77@gmail.com>
2023-12-03 22:16:38 +0600

    best practice looping added

--- python_for_Django/looping.py
+++ python_for_Django/looping.py
@@ -54,3 +54,115 @@ fruit_list = ['apple', 'banana', 'kiwi', 'apple', 'banana', 'orange', 'grape'];
 
 # for i in reversed(sorted(fruit_num)):
 #     print(i)
+
+# for num in range(10):
+#     print(num);
+
+# sum = 0;
+# for i in range(2, 22, 2):
+#     sum = sum + i;
+# print(sum);
+
+# numbers = [1, 2, 3, 4, 5];
+# for i in numbers:
+#     sqare = i ** 2;
+#     print("sqare", i, "in", sqare);
+
+# for i in range(1, 11):
+#     if i % 2 == 0:
+#         print("Even Number: ", i);
+#     else:
+#         print("Odd Number: ", i);
+
+# for i in range(9, 51):
+#     if i % 2 == 0:
+#       print(i);
+
+# numbers = [1, 4, 7, 8, 15, 20, 35, 45, 55];
+# for i in numbers:
+#     if i > 15:
+#         break
+#     else:
+#         print(i)
+
+# for i in range(1, 6):
+#     print(i);
+# else:
+#     print("Done");
+
+# for i in range(1, 6):
+#     if i > 2:
+#         break;
+#     else:
+#         print(i);
+# else:
+#     print("Done");
+
+# list1 = [20, 20, 10, 40];
+# for i in reversed(list1):
+#     print(i);
+# for i in sorted(list1):
+#     print(i);
+
+# numbers = [1, 2, 3, 4];
+# # for i in numbers[1:3]:
+# for i in numbers[::-1]:
+#     print(i);
+
+# name = "Jessa";
+# for i in name:
+#     print(i, end=' ');
+
+# for i in name[::-1]:
+#     print(i, end=' ');
+
+# name = "mohammad ibrahim";
+# for i in name[2:9:2]:
+#  print(i);
+
+# dialogue = "Remember, Red, hope is a good thing, maybe the best of things, and no good thing ever dies";
+# for word in dialogue.split():
+#     print(word);
+
+# dict1 = {"Brand": "BMW", "Color": "Black", "Date": 1964};
+# for key in dict1:
+#     print(key);
+# for value in dict1.items():
+#     print(value);
+
+# dict1 = {"Brand": "BMW", "Color": "Black", "Date": 1964};
+# for value in dict1.values():
+#     print(value);
+# for key in dict1.keys():
+#     print(key);
+# for key,value in dict1.items():
+#     print(key, ':',value)
+# for allItem in dict1.items():
+#     print(allItem);
+
+# num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+# for i in num:
+#         print(i);
+# i = 1;
+# while i <= 10:
+#     print(i)
+#     i += 1;
+
+# for i in range(1, 11):
+#     print(i * 2);
+
+# numbers = [12, 75, 150, 180, 145, 525, 50];
+# for i in numbers:
+#     if i > 500:
+#         break;
+#     elif i < 150:
+#         continue;
+#     elif i % 5 == 0:
+#        print(i);
+
+# for i in range(5):
+# #     print(i);
+# # print("Done");
+
+# for num in range(2,-5,-1):
+#     print(num, end=", ")
