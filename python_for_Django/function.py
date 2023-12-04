5bd83b64de2939af29322617b5bc0ec3e6bfdf23
ibrahimohid <ibrahimohid77@gmail.com>
2023-12-04 20:29:57 +0600

    function added

---
+++ python_for_Django/function.py
@@ -0,0 +1,25 @@
+# def name(fname, lastname):
+#     print(f'Full Name: {fname} {lastname}');
+#
+# name('Mohammad','Ibrahim');
+
+# def person(name, age):
+#     print(f'Name: {name}, Age: {age}');
+# person('Ibrahim', 27);
+
+# def studentInfo(*details):
+#     print(details);
+# studentInfo("Mohammad", '27', "Bangladesh", 3.05, 5.11);
+
+# def name(fname='Mohammad', lname='Ibrahim'):
+#     print(f'Full Name: {fname} {lname}');
+# name('Ohid');
+
+# def student(**studentInfo):
+#     print(studentInfo["fname"]);
+# student(fname="Mohammad", lname= "Ibrahim", age= 25);
+
+# def details(*student, **studentInfo):
+#     print(student,studentInfo);
+# details('Mohammad', 'Ibrahim', 'ohid', fname='Hosna', lname='Mobarak', age= 25);
+
