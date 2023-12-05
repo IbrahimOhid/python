3bbcb1699a0557928fa4e6839af6c53afbbba4d4
ibrahimohid <ibrahimohid77@gmail.com>
2023-12-05 21:07:37 +0600

    range function added

---
+++ python_for_Django/range.py
@@ -0,0 +1,53 @@
+# num = [1, 2, 3, 4, 5, 6];
+# for i in num:
+#     print(i, end=' ');
+
+# for i in range(10, 20):
+#     print(i, end=' ');
+
+# for i in range(5, 51, 5):
+#     print(i, end=' ');
+
+# for i in range(9, 100):
+#     if i % 3 == 0:
+#         print(i, end=' ')
+
+# for i in range(9, 100, 3):
+#     print(i, end=' ');
+
+# for i in range(1, 10, 2):
+#     print("Current Value Of I is :", i);
+
+# for num in range(4):
+#     for i in range(num):
+#         print("*", end=' ');
+#     print()
+
+# for i in range(20, 30, -2):
+#     print(i, end=' ')
+
+# i = 1;
+# while i <= 10:
+#     print(i)
+#     i += 1
+
+# for i in range(1, 6, 1):
+#     for num in range(1, i + 1):
+#         print(num, end=' ');
+#     print()
+
+# for i in range(1, 6):
+#     for j in range(1, i + 1):
+#         print(j, end=' ');
+#     print()
+
+# num = 0;
+# for i in range(1, 11):
+#     num += i;
+# print(num)
+
+# sum = 0;
+# for i in range(1, 11):
+#     sum += i
+# print(sum)
+
