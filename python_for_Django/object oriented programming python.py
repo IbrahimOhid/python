b359a2a8cab5ad9860539555114038f72976df68
ibrahimohid <ibrahimohid77@gmail.com>
2023-12-07 21:14:24 +0600

    OOP practice

--- python_for_Django/object oriented programming python.py
+++ python_for_Django/object oriented programming python.py
@@ -48,3 +48,32 @@
 #
 # obj = C('Ibrahim', 'Developer')
 # obj.title()
+
+class parent:
+    def __init__(self, f_name, m_name):
+        self.father_name = f_name
+        self.mother_name = m_name
+
+    def parent_info(self):
+        print(f"Father Name: {self.father_name}, Mother Name: {self.mother_name}");
+
+
+# p1 = parent("Mohammad Hanif", "Mostora Begum");
+# p1.parent_info()
+
+
+class Child(parent):
+    def __init__(self, f_name, m_name, c_name, edu):
+        super().__init__(f_name, m_name)
+        self.child_name = c_name;
+        self.education = edu;
+
+    def child_info(self):
+        print(
+            f'My Father Name is : {self.father_name}, My Mother Name is : {self.mother_name}, Child Name: {self.child_name},Child Education Level: {self.education}');
+
+
+child1 = Child('Mohammad  Hanif', 'Mostora Begum', 'Ibrahim', 'Diploma in Mechanical Engineering');
+child2 = Child('Mohammad  Hanif', 'Mostora Begum', 'Ebnol', 'HSC');
+child1.child_info();
+child2.child_info();
