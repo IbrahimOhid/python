1f5b07c9f7ca1ad5cad05ed36e66322a0956e70b
ibrahimohid <ibrahimohid77@gmail.com>
2023-12-07 22:24:34 +0600

    python module added

---
+++ python-module/python_module.py
@@ -0,0 +1,13 @@
+# import  math
+#
+# ceil = math.ceil(4.6);
+# print(ceil);
+#
+# floor = math.floor(3.9);
+# print(floor);
+
+import copy
+
+print(copy.add(5, 3));
+print(copy.pi);
+obj = copy.A();
---
+++ python_for_Django/list.py
@@ -0,0 +1,45 @@
+# positive Indexing ⏬
+
+# fruit = ["Banana", "orange", "Grape", "Apple"];
+# print(fruit[3]);
+# fruit.append("Kiw");
+# print(fruit)
+
+# negative indexing ⏬
+
+# countries = ["Bangladesh", "India", "pakistan", "Afganistan", "America", "nederland"];
+# print(countries[-3])
+
+# slicing ⏬
+
+# countries = ["Bangladesh", "Pakistan", "Afganistan", "USA", "Brazil", "Germany", "Argentina", "UAE"];
+# print(countries[2:7])
+# print(countries[::3])
+# print(countries[1:4:2])
+# print(countries[-6:-1:2])
+
+# list method ⏬
+
+# student_name = ["Mohammad", "Ibrahim", "Ohid", "Jisan", "Ebnol"];
+# student_name.append("kaiser");
+# student_name.append(["Mobarak", "Osman", "Faruk"])
+# student_name.pop()
+# student_name.extend(["Mobarak", "Osman", "Faruk"])
+# student_name.insert(2, "rifa")
+# student_name.pop(3)
+# student_name.remove("Ibrahim")
+# print(student_name)
+
+# list sort ⏬
+
+# name = ["Mohammad", "Ibrahim", "Ohid", "Jisan", "Ebnol","brown", "Zannat"];
+# name.sort();
+# name.reverse();
+# name.sort(key=str.lower);
+# name.sort(key=str.upper, reverse=True);
+# print(name);
+
+# num = [1, 13, 2, 88, 99, 2, 5];
+#  num.sort();
+# num.reverse();
+# print(num);
