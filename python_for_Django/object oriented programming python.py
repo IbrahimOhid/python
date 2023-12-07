5e713850ed6894d2cedcdfa12370b9dae3206fe0
ibrahimohid <ibrahimohid77@gmail.com>
2023-12-07 22:06:57 +0600

    OOP best practice

--- python_for_Django/object oriented programming python.py
+++ python_for_Django/object oriented programming python.py
@@ -49,31 +49,42 @@
 # obj = C('Ibrahim', 'Developer')
 # obj.title()
 
-class parent:
-    def __init__(self, f_name, m_name):
-        self.father_name = f_name
-        self.mother_name = m_name
 
-    def parent_info(self):
-        print(f"Father Name: {self.father_name}, Mother Name: {self.mother_name}");
+# class Person:
+#     def __init__(self, name, sex, profession, study):
+#         self.name = name;
+#         self.sex = sex;
+#         self.profession = profession;
+#         self.study = study;
+#
+#     def state(self):
+#         print(f"Name: {self.name}, Sex: {self.sex}, Profession: {self.profession}")
+#
+#     def behavior(self):
+#         print(f"{self.name} working as a {self.profession} and She studies {self.study} a day")
+#
+#
+# people1 = Person("Mohammad", "Male", "Software Developer", "2 Hours");
+# people1.state()
+# people1.behavior()
+#
+# people1 = Person("Ebnol", "Male", "Doctor", "5 Hours");
+# people1.state()
+# people1.behavior()
+
+class student:
+    school_name = 'ACD School';
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def student_info(self):
+        print(f'Name: {self.name} Age: {self.age} School: {student.school_name}')
 
 
-# p1 = parent("Mohammad Hanif", "Mostora Begum");
-# p1.parent_info()
 
 
-class Child(parent):
-    def __init__(self, f_name, m_name, c_name, edu):
-        super().__init__(f_name, m_name)
-        self.child_name = c_name;
-        self.education = edu;
+student1 = student('Mohammad', 15)
+student1.student_info()
 
-    def child_info(self):
-        print(
-            f'My Father Name is : {self.father_name}, My Mother Name is : {self.mother_name}, Child Name: {self.child_name},Child Education Level: {self.education}');
-
-
-child1 = Child('Mohammad  Hanif', 'Mostora Begum', 'Ibrahim', 'Diploma in Mechanical Engineering');
-child2 = Child('Mohammad  Hanif', 'Mostora Begum', 'Ebnol', 'HSC');
-child1.child_info();
-child2.child_info();
